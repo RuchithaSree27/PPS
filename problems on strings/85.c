128b8a7a817661bbb2f245aab05e5251a9f8bca7
Ruchitha  Sree <85932988+RuchithaSree27@users.noreply.github.com>
2021-07-18 17:10:59 +0530

    Add files via upload

---
+++ problems on strings/85.c
@@ -0,0 +1,22 @@
+//program to print multi-line strings
+
+#include<stdio.h>
+int main()
+{
+	int i;
+	char str[100];
+	char ch='@';
+	printf("\nEnter a paragraph and end it with $ :\n");
+
+	for(i=0;ch!='$';i++)
+	{
+	
+	ch = getchar();
+	str[i]=ch;
+	
+   }
+   str[--i] = '\0';
+   printf("\n\n The string is \n");
+   puts(str);
+   
+	return 0;}
